7ae72d9e4f533802a2ec6209718e98bca160b69f
tim-keriazes <tim.keriazes@gmail.com>
2022-05-31 11:05:47 -0700

    control structures exercise

--- data_types_operators_and_variables.py
+++ data_types_operators_and_variables.py
@@ -113,11 +113,35 @@ type({'a':[]})
 
 rented_movies=X #define variable
 x={"littlemermaid":3,"brotherbear":5,"hercules":1} #variable is a dict
-price = sum(x.values()) # sum elements in dict
-print(price) #print
+price = sum(x.values())*3 # sum elements in dict
+print(price) #print (27)
 
 # Suppose you're working as a contractor for 3 companies: Google, Amazon 
 # and Facebook, they pay you a different rate per hour. Google pays 400 
 # dollars per hour, Amazon 380, and Facebook 350. How much will you receive 
 # in payment for this week? You worked 10 hours for Facebook, 6 hours for 
 # Google and 4 hours for Amazon.
+#6530
+
+
+
+
+
+
+
+
+
+# A student can be enrolled to a class only if the class is not 
+# full and the class schedule does not conflict with her current schedule.
+#true
+
+class_has_room= True
+Schedule_works= True
+can_be_enrolled = class_has_room and Schedule_works
+print(can_be_enrolled)
+
+
+# A product offer can be applied only if people buys more than 2 items, 
+# and the offer has not expired. Premium members do not need to buy a 
+# specific amount of products.
+#
