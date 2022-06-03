fa06ec3e3ffec251b790ea1d39ea7ec7d4571708
tim-keriazes <105242897+tim-keriazes@users.noreply.github.com>
2022-06-03 07:38:39 -0700

    Update import_exercises.py

--- import_exercises.py
+++ import_exercises.py
@@ -9,19 +9,56 @@ import imported_func
 # Run an interactive python session and import the module. Call the is_vowel function using the . syntax.
 
 imported_func.is_vowel('a')
+#true
+imported_func.is_vowel('k')
+#false
+imported_func.is_vowel('apple')
+#false
 
 
 
 
 # Create a file named import_exericses.py. Within this file, use from to import the calculate_tip function directly. Call this function with values you choose and print the result.
+
+imported_func.calculate_tip(.2, 100)
+#20
+imported_func.calculate_tip(.45, 1843)
+#829.35
+
 # Create a jupyter notebook named import_exercises.ipynb. Use from to import the get_letter_grade function and give it an alias. Test this function in your notebook.
+
+import imported_func as f
+f.get_letter_grade(65)
+#'D'
+
 # Make sure your code that tests the function imports is run from the same directory that your functions exercise file is in.
 
 # Read about and use the itertools module from the python standard library to help you solve the following problems:
 
+import itertools
+
 # How many different ways can you combine the letters from "abc" with the numbers 1, 2, and 3?
+list(itertools.product("abc", [1, 2, 3]))
+
+# [('a', 1),
+#  ('a', 2),
+#  ('a', 3),
+#  ('b', 1),
+#  ('b', 2),
+#  ('b', 3),
+#  ('c', 1),
+#  ('c', 2),
+#  ('c', 3)]
+# can combine nine different ways
+
 # How many different combinations are there of 2 letters from "abcd"?
+len(list(itertools.combinations('abcd',2)))
+#6 combinations
+
 # How many different permutations are there of 2 letters from "abcd"?
+len(list(itertools.permutations('abcd',2)))
+#12 permutations
+
 # Save this file as profiles.json inside of your exercises directory (right click -> save file as...).
 
 # Use the load function from the json module to open this file.
