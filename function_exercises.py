94ad17948f9fb1e570a504a24089894868462186
tim-keriazes <105242897+tim-keriazes@users.noreply.github.com>
2022-06-02 17:14:14 -0700

    Update function_exercises.py

--- function_exercises.py
+++ function_exercises.py
@@ -57,39 +57,81 @@ def apply_discount(price, discount_percentage):
 # is a number that contains commas in it as input, and return a number as 
 # output.
 
-
+def handle_commas(number_string):
+    string_without_commas = number_string.replace(',','')
+    actual_number = int(string_without_commas)
+    return actual_number
+#handle_commas('1,293,239,239,244')
+#1293239239244
+#could check and confirm with type(handle_commas('1,293,239,239,244'))
 
 # Define a function named get_letter_grade. It should accept a number and 
 # return the letter grade associated with that number (A-F).
 
-
+def get_letter_grade(grade):
+    if type(grade) == int or type(grade) == float:
+        if grade >=90:
+            return 'A'
+        elif grade >= 80:
+            return 'B'
+        elif grade >= 70:
+            return 'C'
+        elif grade >= 60:
+            return 'D'
+        else:
+            return 'F'
+    else:
+        return " Grade must be a number"
+    
+# get_letter_grade(69)
+# 'D'
 
 # Define a function named remove_vowels that accepts a string and 
 # returns a string with all the vowels removed.
 
-
+def is_vowel(string):
+    return len(string) == 1 and string.lower() in 'aeiou'
+def is_consonant(string):
+    return len(string) == 1 and not is_vowel(string) and string.isalpha()
+def remove_vowels(string):
+    output = ''
+    for char in string:
+        if is_consonant(char):
+            output += char
+    return output
 
 # Define a function named normalize_name. It should accept a string and 
-# return a valid python identifier, that is:
-
-
-
+# return a valid python identifier, that has no whitespace, lowercase, valid identifier:
 # anything that is not a valid python identifier should be removed
 # leading and trailing whitespace should be removed
 # everything should be lowercase
 # spaces should be replaced with underscores
 
+def remove_special_char(string):
+    return ''.join([c for c in string if c.isalnum() or c == ' '])
 
 
-# for example:
-# Name will become name
-# First Name will become first_name
-# % Completed will become completed
+def normalize_name(string):
+    special_char_removed = remove_special_char(string)
+    return special_char_removed.lower().strip().replace(' ', '_')
+
+# normalize_name('s,uper funny norma,l')
+#'super_funny_normal'
+
+
 # Write a function named cumulative_sum that accepts a list of numbers and returns a list that is the cumulative sum of the numbers in the list.
 # cumulative_sum([1, 1, 1]) returns [1, 2, 3]
 # cumulative_sum([1, 2, 3, 4]) returns [1, 3, 6, 10]
 
-
+def cumulative_sum(nums):
+    output = []
+    total = 0
+    for num in nums:
+        total += num
+        output.append(total)
+    return output
+#cumulative_sum([1,2,3])
+#[1, 3, 6]
 
 # Additional Exercise
 
